312358fe60d86a4cffc0c86793f895b8b237b0f8
Rcorvo <rcu1987@gmail.com>
2016-09-27 16:53:12 +0200

    Fin del ejercicio 2.1

--- app/src/main/java/com/example/raulcorvo/holadam/MainActivity.java
+++ app/src/main/java/com/example/raulcorvo/holadam/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.raulcorvo.holadam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        TextView nuevoTexto = new TextView(this);
+        nuevoTexto.setText("Hola DAM desde Java");
+        setContentView(nuevoTexto);
     }
 }
